bfd78bd0fa8a8c4b5e689ec67fbbeff79daee4bb
jekasrs <jekasjeny1012@gmail.com>
2024-04-09 19:27:27 +0300

    installed paramiko, realised an example of connection

--- main.py
+++ main.py
@@ -1,3 +1,15 @@
+import paramiko
+
+host = 'localhost'
+user = 'root'
+password = 'root'
+port = 2222
 
 if __name__ == '__main__':
-    print(1)
+    client = paramiko.SSHClient()
+    client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    client.connect(hostname=host, username=user, password=password, port=port)
+    stdin, stdout, stderr = client.exec_command('ls -a')
+    data = stdout.read().decode("utf-8")
+    print(data)
+    client.close()
